494e40c1343781f790f606b7b98c8bbf7429786c
Bevanwhite <jefferybevan@gmail.com>
2024-12-03 00:02:39 +0530

    server component is changed

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -3,9 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-servers',
   standalone: false,
-
-  templateUrl: `<app-server></app-server><app-server></app-server>`,
-  // templateUrl: './servers.component.html',
+  templateUrl: './servers.component.html',
   styleUrl: './servers.component.css',
 })
 export class ServersComponent {}
