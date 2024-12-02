635531b7483838f5d0bfaab8ab1e0012daf26c76
Bevanwhite <jefferybevan@gmail.com>
2024-12-02 18:50:43 +0530

    complete upto assignment

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -3,10 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-servers',
   standalone: false,
-  
-  templateUrl: './servers.component.html',
-  styleUrl: './servers.component.css'
-})
-export class ServersComponent {
 
-}
+  templateUrl: `<app-server></app-server><app-server></app-server>`,
+  // templateUrl: './servers.component.html',
+  styleUrl: './servers.component.css',
+})
+export class ServersComponent {}
